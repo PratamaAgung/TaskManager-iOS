f933ab55981fd2be59bc4418bed3fd55481b4098
PratamaAgung <apratamamia@gmail.com>
2019-04-30 09:51:45 +0700

    Initial Commit

---
+++ TaskManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskManager
+//
+//  Created by JojoBandung on 4/30/19.
+//  Copyright © 2019 JojoBandung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
